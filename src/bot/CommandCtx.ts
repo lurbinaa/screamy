1c9ddd5d919f90ea968fb75d3346ac28fb447a56
Leonardo Urbina <apinky549@gmail.com>
2025-11-04 10:55:49 -0500

    feat(Bot): add defer, follow up and getContent helper

--- src/bot/CommandCtx.ts
+++ src/bot/CommandCtx.ts
@@ -1,6 +1,8 @@
 import {
     Constants,
+    type Message,
     type CommandInteraction,
+    type InteractionContent,
 } from "eris";
 import i18next from "i18next";
 import type {
@@ -10,31 +12,61 @@ import type {
     CtxReplyFile
 } from "@lib/types";
 
+const { EPHEMERAL } = Constants.MessageFlags
+
 export class CommandCtx {
     public constructor(public interaction: CommandInteraction, public locale: string | null) { };
 
-    public async reply(content: CtxReplyContent, tOptions?: CtxTOptions, file?: CtxReplyFile) {
-        const contentIsString = typeof content === 'string';
-        const key = contentIsString ? content : content.content ?? 'bot.errors.no_content';
-        const tContent = i18next.t(key, { lng: this.locale ?? undefined, ...tOptions });
-        const _content = contentIsString ? tContent : { content: tContent, ...content };
-
+    public async reply(content: CtxReplyContent, tOptions?: CtxTOptions, file?: CtxReplyFile): Promise<void> {
+        const _content = this.getContent(content, false, tOptions);
         return this.interaction.createMessage(_content, file);
     }
 
-    public async silentReply(content: CtxSilentReplyContent, tOptions?: CtxTOptions, file?: CtxReplyFile) {
+    public async silentReply(content: CtxSilentReplyContent, tOptions?: CtxTOptions, file?: CtxReplyFile): Promise<void> {
+        const _content = this.getContent(content, true, tOptions);
+        return this.interaction.createMessage(_content, file);
+    }
+
+    public async defer(silent?: boolean): Promise<void> {
+        const flags = silent ? EPHEMERAL : undefined;
+        return this.interaction.defer(flags);
+    }
+
+    public async followUp(content: CtxSilentReplyContent, tOptions?: CtxTOptions, file?: CtxReplyFile): Promise<Message> {
+        const _content = this.getContent(content, null, tOptions);
+        return this.interaction.createFollowup(_content, file);
+    }
+
+    private getContent(
+        content: CtxReplyContent,
+        silent: null,
+        tOptions?: CtxTOptions,
+    ): InteractionContent
+    private getContent(
+        content: CtxReplyContent,
+        silent: false,
+        tOptions?: CtxTOptions,
+    ): InteractionContent
+    private getContent(
+        content: CtxSilentReplyContent,
+        silent: true,
+        tOptions?: CtxTOptions,
+    ): InteractionContent
+    private getContent(
+        content: CtxSilentReplyContent | CtxReplyContent,
+        silent: boolean | null,
+        tOptions?: CtxTOptions,
+    ): InteractionContent {
         const contentIsString = typeof content === 'string';
         const key = contentIsString ? content : content.content ?? 'bot.errors.no_content';
         const tContent = i18next.t(key, { lng: this.locale ?? undefined, ...tOptions });
-        const _content = contentIsString ? {
+        return contentIsString ? {
             content: tContent,
-            flags: Constants.MessageFlags.EPHEMERAL
+            flags: silent ? EPHEMERAL : undefined
         } : {
             content: tContent,
-            flags: Constants.MessageFlags.EPHEMERAL,
+            flags: silent ? EPHEMERAL : undefined,
             ...content
         };
-
-        return this.interaction.createMessage(_content, file);
     }
 }
--- src/bot/commands/ping.ts
+++ src/bot/commands/ping.ts
@@ -2,12 +2,14 @@ import { CommandCtx } from "@bot";
 import { manager } from "@manager";
 
 export default {
-    description: "Ping command",
+    description: 'Ping command',
     execute: async (context: CommandCtx): Promise<void> => {
+        await context.defer();
+
         const { postgres, redis } = await manager.database.ping();
         const discord = isFinite(manager.bot.latency) ? `${manager.bot.latency}ms` : 'pending...';
 
-        return context.reply('bot.commands.ping.pong', {
+        await context.followUp('bot.commands.ping.pong', {
             discord,
             postgres: postgres.toFixed(),
             redis: redis.toFixed()
